a68416b7c07d794c8a63ae0056792669da71edca
windows10 <2380253499@qq.com>
2018-12-29 15:35:03 +0800

    项目文件

--- app/src/main/java/com/test/fastshape/MainActivity.java
+++ app/src/main/java/com/test/fastshape/MainActivity.java
@@ -1,6 +1,5 @@
 package com.test.fastshape;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -28,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                startActivity(new Intent(MainActivity.this,TestLinearlayoutActivity.class));
+//                startActivity(new Intent(MainActivity.this,TestLinearlayoutActivity.class));
             }
         });
 
@@ -44,19 +43,14 @@ public class MainActivity extends AppCompatActivity {
         viewHelper.complete();//或者textView.complete();
 
         MyLinearLayout linearLayout=new MyLinearLayout(this);
-        linearLayout.clearAttr()
-                .setAll_line(true)
-                .setGradientType(BaseViewHelper.gradientType_linear)
-                .setSolidColor(ContextCompat.getColor(this,R.color.white))
-                .setRadius(20)
-                .complete();
-        /*linearLayout.getViewHelper()
+
+        linearLayout.getViewHelper()
                 .clearAttribute()
                 .setAllLine(true)
                 .setGradientType(BaseViewHelper.gradientType_linear)
                 .setSolidColor(ContextCompat.getColor(this,R.color.white))
                 .setRadius(20)
-                .complete();//或者linearLayout.complete();*/
+                .complete();//或者linearLayout.complete();
 
     }
 }
