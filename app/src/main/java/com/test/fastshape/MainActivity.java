f3da7272dc963b7e13c550c92676792b9c8dcccb
Administrator <zr2013123@sina.com>
2019-01-29 17:41:47 +0800

    update file

--- app/src/main/java/com/test/fastshape/MainActivity.java
+++ app/src/main/java/com/test/fastshape/MainActivity.java
@@ -19,19 +19,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.tvLook).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+             /*   Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
                 startActivity(new Intent(MainActivity.this, ShapeTestListActivity.class));
-//                startActivity(new Intent(MainActivity.this,TestLinearlayoutActivity.class));
-//            }
-//        });
-
             }
-
         });
     }
 }
